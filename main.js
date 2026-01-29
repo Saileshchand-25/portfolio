15bfc6f256eec447c237b31031af4cd7b203a88c
Sailesh Chand <saileshchand2025@gmail.com>
2026-01-29 15:35:39 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    new Typed(".text", {
+        strings: ["Frontend Developer", "Web Designer", "React Developer"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+});
